9939d2176e39545d5054b03922ae1e3e4c79a460
Joshua Owusu <joshbillion1@gmail.com>
2023-08-01 21:49:34 +0000

    Commit auto_update

--- aaaa/app.js
+++ aaaa/app.js
@@ -1,38 +1,38 @@
 const maskedText = document.getElementById("maskedText");
-      const mask = document.getElementById("mask");
+const mask = document.getElementById("mask");
 
-      let isHovered = false;
-      let mouseX = 0;
-      let mouseY = 0;
+let isHovered = false;
+let mouseX = 0;
+let mouseY = 0;
 
-      maskedText.addEventListener("mouseenter", () => {
-        isHovered = true;
-      });
+maskedText.addEventListener("mouseenter", () => {
+  isHovered = true;
+});
 
-      maskedText.addEventListener("mouseleave", () => {
-        isHovered = false;
-      });
+maskedText.addEventListener("mouseleave", () => {
+  isHovered = false;
+});
 
-      function useMousePosition() {
-        return { x: mouseX, y: mouseY };
-      }
+function useMousePosition() {
+  return { x: mouseX, y: mouseY };
+}
 
-      mask.addEventListener("mousemove", (e) => {
-        const rect = mask.getBoundingClientRect();
-        mouseX = e.clientX - rect.left;
-        mouseY = e.clientY - rect.top;
-      });
+mask.addEventListener("mousemove", (e) => {
+  const rect = mask.getBoundingClientRect();
+  mouseX = e.clientX - rect.left;
+  mouseY = e.clientY - rect.top;
+});
 
-      function animateMask() {
-        const { x, y } = useMousePosition();
-        const size = isHovered ? 400 : 40;
+function animateMask() {
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
 
-        gsap.timeline().to(mask, {
-          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
-          WebkitMaskSize: `${size}px`,
-          duration: 0.5,
-          ease: "back.out(4)",
-        });
-      }
+  gsap.timeline().to(mask, {
+    WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+    WebkitMaskSize: `${size}px`,
+    duration: 0.5,
+    ease: "back.out(4)",
+  });
+}
 
-      gsap.ticker.add(animateMask);
+gsap.ticker.add(animateMask);
