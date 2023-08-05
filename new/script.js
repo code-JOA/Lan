e9d683289f1597c890aef68eed063b02d32bc166
Joshua Owusu <joshbillion1@gmail.com>
2023-08-05 06:50:50 +0000

    Commit auto_update

--- new/script.js
+++ new/script.js
@@ -1,15 +1,15 @@
 // rotating circle
-// const text = document.querySelector(".text p");
-// text.innerHTML = text.innerText
-//   .split("")
-//   .map(
-//     (char, i) => `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
-//   )
-//   .join("");
-
 const text = document.querySelector(".text p");
-text.innerHTML = [...text.innerText]
+text.innerHTML = text.innerText
+  .split("")
   .map(
     (char, i) => `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
   )
   .join("");
+
+// const text = document.querySelector(".text p");
+// text.innerHTML = [...text.innerText]
+//   .map(
+//     (char, i) => `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
+//   )
+//   .join("");
