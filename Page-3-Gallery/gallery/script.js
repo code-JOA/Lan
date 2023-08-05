ab3dcc875852ead285adb766cf419591b02dc67f
Joshua Owusu <joshbillion1@gmail.com>
2023-08-05 05:33:52 +0000

    Commit auto_update

--- Page-3-Gallery/gallery/script.js
+++ Page-3-Gallery/gallery/script.js
@@ -71,10 +71,7 @@ document.querySelector("#aboutme").addEventListener("mouseleave", function () {
 
 // rotating circle
 const text = document.querySelector(".text p");
-text.innerHTML = text.innerText
-  .split("")
-  .map(
-    char,
-    (i) => `<span style="transform:rotate(${i * 8.3}deg)">$(char)</span>`
+text.innerHTML = text.innerText.split("").map(
+    char,i) => `<span style="transform:rotate(${i * 8.3}deg)">$(char)</span>`
   )
   .join("");
